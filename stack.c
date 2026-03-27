5e59cc8d6b82a1a34b979c0c5e37425d7cc3fb3d
Priyanshu Singh <priyanshusingh280906@gmail.com>
2026-03-27 13:06:16 +0530

    first commit

---
+++ stack.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow! Cannot push %d\n", value);
+    } else {
+        top++;
+        stack[top] = value;
+        printf("Pushed %d onto the stack.\n", value);
+    }
+}
