065ffc0c90fe418b3f53c90aba34e1787572dede
Priyanshu Singh <priyanshusingh280906@gmail.com>
2026-03-27 22:52:48 +0530

    final commit for linkedlist

--- linklist.c
+++ linklist.c
@@ -8,3 +8,52 @@ struct Node {
 
 struct Node* head = NULL;
 
+void insert(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+
+    printf("%d inserted into linked list\n", value);
+}
+
+void display() {
+    struct Node* temp = head;
+
+    if (temp == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    printf("Linked List:\n");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n--- Linked List Menu ---\n");
+        printf("1. Insert\n2. Display\n3. Exit\n");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                insert(value);
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
