727a2f9253cf13b1404eff2dd2279d881d273021
Priyanshu Singh <priyanshusingh280906@gmail.com>
2026-03-27 15:30:19 +0530

    added queue structure with front and rear pointers

---
+++ queue.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5  // Defining the maximum size of the queue
+
+int queue[MAX];
+int front = -1;  // Initializing front to -1 (empty queue)
+int rear = -1;   // Initializing rear to -1 (empty queue)
